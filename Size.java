6a6fefcac6dfeb5b471fc8b80ebb35743f978e8e
Bruno Silva <200221046@estudantes.ips.pt>
2021-03-04 16:44:12 +0000

    Documentação

--- Size.java
+++ Size.java
@@ -1,3 +1,7 @@
+/**
+ * Este enumerado contêm as caraterísticas de cada chávena, sendo que existem 3 tipos, SMALL, MEDIUM e LARGE.
+ * 
+ */
 public enum Size {
 
     SMALL(32, 36, "Pequeno",'S'),
@@ -8,7 +12,11 @@ public enum Size {
      private final int minValue;
      private final int maxValue;
      private final char code;
-
+    
+     /**
+      * Este construtor recebe os valores dependendo do tipo do enumerado Size.
+      * Dá valores aos atributos do enumerdo Size.
+      */
     private Size(int minValue, int maxValue, String description, char code){
        this.description=description;
        this.minValue=minValue;
@@ -16,23 +24,48 @@ public enum Size {
        this.code=code;
     }
     
+    /**
+     * Este método retorna a descrição do tamanho (Size) da chávena
+     * 
+     * @return description
+     */
    @Override
     public String toString() {
       return this.description;
     }
     
+    /**
+     * Este método retorna a descrição do tamanho (Size) da chávena
+     * 
+     * @return description
+     */
     public String getDescription() {
        return description;
     }
     
+    /**
+     * Este método retorna o valor mínimo para o tamanho (Size) da chávena
+     * 
+     * @return minValue
+     */
     public int getMinValue() {
        return minValue;
     }
     
+    /**
+     * Este método retorna o valor máximo para o tamanho (Size) da chávena
+     * 
+     * @return maxValue
+     */
     public int getMaxValue() {
        return maxValue;
     }
     
+    /**
+     * Este método retorna o código atríbuido ao tamanho (Size) da chávena
+     * 
+     * @return code
+     */
     public char getCode() {
        return code;
     }
